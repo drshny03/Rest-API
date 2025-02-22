c170c289e5f20f5628a993373b1fc56d818641af
drshny03 <darshanyadav7049@gmail.com>
2025-02-22 10:55:38 +0530

    [Darshan] Merge branch into main

---
+++ src/main/java/com/capgemini_training/RestCallApi/controller/HelloRestController.java
@@ -0,0 +1,14 @@
+package com.capgemini_training.RestCallApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+    @GetMapping
+    public String hello() {
+        return "Hello from BridgeLabz";
+    }
+}
